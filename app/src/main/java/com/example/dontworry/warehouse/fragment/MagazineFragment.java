f5c1e3b067fdd0739f74e71f4d69e266b93a3798
saygogo <1183769411@qq.com>
2017-07-05 21:43:09 +0800

    实现底部RedioButton的切换+1

---
+++ app/src/main/java/com/example/dontworry/warehouse/fragment/MagazineFragment.java
@@ -0,0 +1,30 @@
+package com.example.dontworry.warehouse.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Don't worry on 2017/7/5.
+ */
+
+public class MagazineFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("杂志页面");
+        return textView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
